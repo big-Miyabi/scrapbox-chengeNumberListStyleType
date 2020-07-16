1f0585959440633aa61e37269fd887ca21c2f486
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-07-17 02:44:34 +0900

    コードはかなり乱雑だが、やりたかった数字付き箇条書きの動的装飾はできた。この後コードの整形と、インデント3の時の処理を書く

--- scrapbox.js
+++ scrapbox.js
@@ -4,7 +4,80 @@ scrapbox.PopupMenu.addButton({
   onClick: (text) => `[[${text}]]`,
 });
 
-const $appRoot = $("#app-container");
+const convertToRoman = (num, isUpper) => {
+  const decimal = [
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    8,
+    7,
+    6,
+    5,
+    4,
+    3,
+    2,
+    1,
+  ];
+  const upperRomanNumeral = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "ⅩC",
+    "L",
+    "ⅩL",
+    "Ⅹ",
+    "Ⅸ",
+    "Ⅷ",
+    "Ⅶ",
+    "Ⅵ",
+    "Ⅴ",
+    "Ⅳ",
+    "Ⅲ",
+    "Ⅱ",
+    "Ⅰ",
+  ];
+  const lowerRomanNumeral = [
+    "m",
+    "cm",
+    "d",
+    "cd",
+    "c",
+    "ⅹc",
+    "l",
+    "ⅹl",
+    "ⅹ",
+    "ⅸ",
+    "ⅷ",
+    "ⅶ",
+    "ⅵ",
+    "ⅴ",
+    "ⅳ",
+    "ⅲ",
+    "ⅱ",
+    "ⅰ",
+  ];
+
+  const romanNumeral = isUpper ? upperRomanNumeral : lowerRomanNumeral;
+
+  let romanized = "";
+
+  for (var i = 0; i < decimal.length; i++) {
+    while (decimal[i] <= num) {
+      romanized += romanNumeral[i];
+      num -= decimal[i];
+    }
+  }
+  return romanized;
+};
 
 const findNumberList = (val) => {
   const text = $(val).text();
@@ -37,21 +110,86 @@ const getTarget = (textVal) => {
 };
 
 const addColor = (target) => {
+  let isFirst = true;
+  let listStyleTypeIndex = 0;
+  let listNumStr = "";
+  const spanArray = [];
+
   $(target)
     .children()
     .each((i, targetSpan) => {
       const text = $(targetSpan).text();
       const isBlank = /^\s$/.test(text);
-
       if (isBlank) return false;
 
-      const isNumberOrDot = /^(\d|\.)+$/.test(text);
-      if (isNumberOrDot) {
-        const span = document.createElement("span");
-        span.textContent = text;
-        span.setAttribute("style", "color: #619FE0");
-        targetSpan.textContent = null;
-        targetSpan.appendChild(span);
+      if (isFirst) {
+        const className = targetSpan.className;
+        const indentAmount = className.substr(2, 1);
+        listStyleTypeIndex = indentAmount % 3;
+        isFirst = false;
+      }
+
+      switch (listStyleTypeIndex) {
+        case 0: {
+          const isNumber = /^\d+$/.test(text);
+          const isDot = /^\.+$/.test(text);
+          if (isNumber || isDot) {
+            const span = document.createElement("span");
+            span.textContent = text;
+            span.setAttribute("style", "color: #619FE0");
+            targetSpan.textContent = null;
+            targetSpan.appendChild(span);
+          }
+          break;
+        }
+        case 1:
+        case 2: {
+          spanArray.push(targetSpan);
+          const isNumber = /^\d+$/.test(text);
+          if (isNumber) listNumStr += text;
+          const isDot = /^\.+$/.test(text);
+          if (isDot) {
+            const listNumDec = Number(listNumStr);
+            const listNumUpperRoman = convertToRoman(listNumDec, true) + ".";
+            listNumStr += ".";
+
+            spanArray.forEach((value, index) => {
+              if (listNumUpperRoman.length > listNumStr.length) {
+                // ローマ数字の桁数が元の数字の桁数よりも大きい時
+                const isLast = listNumStr.length - 1 === index;
+                const span = document.createElement("span");
+                span.textContent = isLast
+                  ? listNumUpperRoman.substr(index)
+                  : listNumUpperRoman.substr(index, 1);
+                span.setAttribute("style", "color: #619FE0");
+                spanArray[index].textContent = null;
+                spanArray[index].appendChild(span);
+              } else if (listNumUpperRoman.length < listNumStr.length) {
+                // ローマ数字の桁数が元の数字の桁数よりも小さい時
+                const start = listNumStr.length - listNumUpperRoman.length;
+                if (index < start) {
+                  spanArray[index].textContent = null;
+                } else {
+                  // index >= start
+                  const span = document.createElement("span");
+                  span.textContent = listNumUpperRoman.substr(index - start, 1);
+                  span.setAttribute("style", "color: #619FE0");
+                  spanArray[index].textContent = null;
+                  spanArray[index].appendChild(span);
+                }
+              } else {
+                // listNumUpperRoman.length === listNumStr.length
+                const span = document.createElement("span");
+                span.textContent = listNumUpperRoman.substr(index, 1);
+                span.setAttribute("style", "color: #619FE0");
+                spanArray[index].textContent = null;
+                spanArray[index].appendChild(span);
+              }
+            });
+
+            return false;
+          }
+        }
       }
     });
 };
@@ -68,6 +206,7 @@ const customizeNumberList = () => {
   });
 };
 
+const $appRoot = $("#app-container");
 $appRoot.on("keyup", (e) => {
   customizeNumberList();
 });
