05cfdfbcbcf30dfb997d8dc5e21cb17bbcee6d30
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-07-17 16:13:10 +0900

    createSpan関数を定義してコードを整形した

--- scrapbox.js
+++ scrapbox.js
@@ -109,6 +109,14 @@ const getTarget = (textVal) => {
   }
 };
 
+const createSpan = (textContent, targetSpan) => {
+  const span = document.createElement("span");
+  span.textContent = textContent;
+  span.setAttribute("style", "color: #619FE0");
+  targetSpan.textContent = null;
+  targetSpan.appendChild(span);
+};
+
 const addColor = (target) => {
   let isFirst = true;
   let listStyleTypeIndex = 0;
@@ -122,6 +130,7 @@ const addColor = (target) => {
       const isBlank = /^\s$/.test(text);
       if (isBlank) return false;
 
+      // orderListのタイプをここで判断
       if (isFirst) {
         const className = targetSpan.className;
         const indentAmount = className.substr(2, 1);
@@ -129,6 +138,7 @@ const addColor = (target) => {
         isFirst = false;
       }
 
+      // 判断したタイプをもとに処理を振り分け
       switch (listStyleTypeIndex) {
         case 0: {
           const isNumber = /^\d+$/.test(text);
@@ -144,51 +154,49 @@ const addColor = (target) => {
         }
         case 1:
         case 2: {
+          // ドットが来るまでspanArrayに処理を追加
           spanArray.push(targetSpan);
           const isNumber = /^\d+$/.test(text);
           if (isNumber) listNumStr += text;
           const isDot = /^\.+$/.test(text);
-          if (isDot) {
-            const listNumDec = Number(listNumStr);
-            const listNumUpperRoman = convertToRoman(listNumDec, true) + ".";
-            listNumStr += ".";
 
-            spanArray.forEach((value, index) => {
-              if (listNumUpperRoman.length > listNumStr.length) {
-                // ローマ数字の桁数が元の数字の桁数よりも大きい時
-                const isLast = listNumStr.length - 1 === index;
-                const span = document.createElement("span");
-                span.textContent = isLast
-                  ? listNumUpperRoman.substr(index)
-                  : listNumUpperRoman.substr(index, 1);
-                span.setAttribute("style", "color: #619FE0");
+          if (!isDot) return true;
+
+          // ドットだった時の処理
+          const listNumDec = Number(listNumStr);
+          const listNumUpperRoman = convertToRoman(listNumDec, true) + ".";
+          listNumStr += ".";
+
+          // ドット以前の数字たちが格納されたspanの配列をforEachで回す
+          // 以前の数字を新しい数字(ローマ数字)で置き換えている
+          spanArray.forEach((value, index) => {
+            if (listNumUpperRoman.length > listNumStr.length) {
+              // ローマ数字の桁数が元の数字の桁数よりも大きい時
+              // 最後の置換位置に残りの超過したローマ数字をぶち込む
+              const isLast = listNumStr.length - 1 === index;
+              const textContent = isLast
+                ? listNumUpperRoman.substr(index)
+                : listNumUpperRoman.substr(index, 1);
+
+              createSpan(textContent, spanArray[index]);
+            } else if (listNumUpperRoman.length < listNumStr.length) {
+              // ローマ数字の桁数が元の数字の桁数よりも小さい時
+              const start = listNumStr.length - listNumUpperRoman.length;
+              if (index < start) {
                 spanArray[index].textContent = null;
-                spanArray[index].appendChild(span);
-              } else if (listNumUpperRoman.length < listNumStr.length) {
-                // ローマ数字の桁数が元の数字の桁数よりも小さい時
-                const start = listNumStr.length - listNumUpperRoman.length;
-                if (index < start) {
-                  spanArray[index].textContent = null;
-                } else {
-                  // index >= start
-                  const span = document.createElement("span");
-                  span.textContent = listNumUpperRoman.substr(index - start, 1);
-                  span.setAttribute("style", "color: #619FE0");
-                  spanArray[index].textContent = null;
-                  spanArray[index].appendChild(span);
-                }
               } else {
-                // listNumUpperRoman.length === listNumStr.length
-                const span = document.createElement("span");
-                span.textContent = listNumUpperRoman.substr(index, 1);
-                span.setAttribute("style", "color: #619FE0");
-                spanArray[index].textContent = null;
-                spanArray[index].appendChild(span);
+                // index >= start
+                const textContent = listNumUpperRoman.substr(index - start, 1);
+                createSpan(textContent, spanArray[index]);
               }
-            });
+            } else {
+              // listNumUpperRoman.length === listNumStr.length
+              const textContent = listNumUpperRoman.substr(index, 1);
+              createSpan(textContent, spanArray[index]);
+            }
+          });
 
-            return false;
-          }
+          return false;
         }
       }
     });
